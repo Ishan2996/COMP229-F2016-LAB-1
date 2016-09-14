fbefcca8c29ca317d9fbd86ad8be2d99d9b5675d
Ishan S Sharma <Ishan S Sharma@DESKTOP-C6LS930>
2016-09-14 19:30:00 -0400

    Add project files.

---
+++ COMP229-Lesson1/Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace COMP229_Lesson1
+{
+    public partial class _Default : Page
+    {
+        /**
+         * <summary>
+         * This Method executes every time the page loads
+         * </summary>
+         */ 
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HelloLabel.Text = "Hello And Good Bye !";
+        }
+    }
+}
